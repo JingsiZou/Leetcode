9c8f35d14962340254fc8a6c3b3460ca53778ff9
JingsiZou <jingsizou.cs@gmail.com>
2021-04-10 23:08:06 +0800

    Add a LearnGithub python file

---
+++ LearnGithub.py
@@ -0,0 +1,23 @@
+print("Hello World!")
+print("Learn Git for the first time")
+"""
+i:从前insert
+a:从后插入
+o：下一行
+O：上一行
+u:撤回
+x:删除一个字母
+d+左右键：剪切一个字母
+dd:剪切一行
+ctrl + r：重做，少撤回一次
+ZZ：保存并退出的快捷键
+冒号+w：保存
+冒号+q：退出
+冒号+q！：强制推出
+冒号+wq：保存并推出
+yy:复制一行
+p：粘贴一行
+v:选择
+V:直接选择一行
+"""
+
